736a1be639f9c227f7e57529b47052c2c180eeeb
Jesus-D-Castellanos <castellanosjesus801@gmail.com>
2024-12-13 13:24:04 -0500

    Add files via upload

---
+++ Codigo/Repositorios/Interfaces/IComidas.cs
@@ -0,0 +1,18 @@
+﻿using RunGym.Models;
+namespace RunGym.Repositorios.Interfaces
+{
+    public interface IComidas
+    {
+        Task<List<Comidas>> GetComidas();
+
+        Task<Comidas> GetComidasById(int id);
+
+        Task<Comidas> GetComidasByName(string nombre);
+
+        Task<bool> PostComidas(Comidas comidas);
+
+        Task<bool> PutComidas(Comidas comidas);
+
+        Task<bool> DeleteComidas(Comidas comidas);
+    }
+}
---
+++ Codigo/Repositorios/Interfaces/IUsuarios.cs
@@ -0,0 +1,11 @@
+﻿using RunGym.Models;
+namespace RunGym.Repositorios.Interfaces
+{
+    public interface IUsuarios
+    {
+        Task<List<Usuarios>> GetUsuarios();
+        Task<bool> PostUsuarios(Usuarios usuarios);
+        Task<bool> PutUsuarios(Usuarios usuarios);
+        Task<bool> DeleteUsuarios(int id);
+    }
+}
