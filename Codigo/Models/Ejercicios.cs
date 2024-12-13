8989b45fc28bb505feae7968e2c451b7fdae93a1
Jesus-D-Castellanos <castellanosjesus801@gmail.com>
2024-12-13 13:21:17 -0500

    Add files via upload

---
+++ Codigo/Models/Ejercicios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGym.Models
+{
+    public class Ejercicios
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int IdRutina { get; set; }
+        public string Nombre_Ejercicio { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
